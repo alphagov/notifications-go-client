07b9c34a2d510dfd1a077a12039b4f70bf94a308
Rafal Proszowski <rafal.proszowski@digital.cabinet-office.gov.uk>
2017-05-08 15:09:14 +0100

    Add testing suite
    The tests will be actively developed and used in this package. This
    suite will cover the tests, when executed with `go test` instead of
    `ginkgo`, being a wrapper providing all the fancy functionality from
    Ginkgo and Gomega library.

---
+++ suite_test.go
@@ -0,0 +1,13 @@
+package notify
+
+import (
+	"testing"
+
+	. "github.com/onsi/ginkgo"
+	. "github.com/onsi/gomega"
+)
+
+func TestMain(t *testing.T) {
+	RegisterFailHandler(Fail)
+	RunSpecs(t, "Main Suite")
+}
